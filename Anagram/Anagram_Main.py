73744588570aa69282a6724c706f5e41c3950fbd
EliButler <40725211+EliButler@users.noreply.github.com>
2021-01-08 11:00:52 -0600

    Added a function to display final results

--- Anagram/Anagram_Main.py
+++ Anagram/Anagram_Main.py
@@ -1,9 +1,26 @@
 "IMPORTS"
-from Kata.Anagram.Word_List import Words
 
-print(Words[0])
+from Kata.Anagram.Word_List import Words as Word_List
+from Kata.Anagram.Word_List import *
 
+"Take in a name and generate a word list that matches the amount of letters."
+name = "eli butler"
 
+"Match the Number letters in name to a combination of letters and words in the word list."
+Matching_List = Name_Number_Match(name)
+# print(len(Matching_List))
+
+"Now I must determine if this has a matching set of letters."
+Letter_Match = Match_Letters(Matching_List, name)
+
+# print(Letter_Match)
+
+"Removing duplicate items."
+No_Dups = Remove_Dups(Letter_Match)
+# print("Duplicate Removal: ", No_Dups)
+
+"""Printing Final Result"""
+Final_Result(name, No_Dups)
 
 
 
